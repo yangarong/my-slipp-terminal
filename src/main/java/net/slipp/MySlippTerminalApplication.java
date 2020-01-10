98f8ae70c3a2f3f639d2d0e2d6cdcaf1d847269b
yangarong <yangarong@gmail.com>
2020-01-10 12:43:55 +0900

    initial commit

---
+++ src/main/java/net/slipp/MySlippTerminalApplication.java
@@ -0,0 +1,13 @@
+package net.slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySlippTerminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySlippTerminalApplication.class, args);
+	}
+
+}
